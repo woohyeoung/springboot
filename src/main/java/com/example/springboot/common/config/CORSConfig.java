cad723db207e01a99d4347fc6356753921205fd0
woohyeoung <dngud951108@gmail.com>
2022-05-29 15:04:50 +0900

    Fix: 05/29, user signUp-Exception test complete to UserControllerTest

--- src/main/java/com/example/springboot/common/config/CORSConfig.java
+++ src/main/java/com/example/springboot/common/config/CORSConfig.java
@@ -12,11 +12,12 @@ public class CORSConfig {
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정하는 것
-		config.addAllowedOriginPattern("*"); // 모든 ip에 응답을 허용함
-		config.addAllowedHeader("*"); // 모든 header에 응답을 허용함
-		config.addAllowedMethod("*"); // 모든 post, get, delete, put 요청 허용함
-		source.registerCorsConfiguration("/**", config); // /api로 들어오는 모든 주소에 config 설정
+		config.setAllowCredentials(true);
+//		config.addAllowedOriginPattern("*");
+		config.addAllowedOrigin("http://localhost:3000");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
 	}
 }
--- src/main/java/com/example/springboot/user/controller/UserController.java
+++ src/main/java/com/example/springboot/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/user")
 public class UserController {
 
 	private final UserService userService;
