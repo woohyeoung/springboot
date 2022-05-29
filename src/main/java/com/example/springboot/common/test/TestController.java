f198a95429d2397990cf74acb7d2bb470442ce9d
woohyeoung <dngud951108@gmail.com>
2022-05-29 14:07:07 +0900

    Fix: 05/29, Modify to TestCase

--- src/main/java/com/example/springboot/common/test/TestController.java
+++ src/main/java/com/example/springboot/common/test/TestController.java
@@ -1,5 +1,7 @@
 package com.example.springboot.common.test;
 
+import com.example.springboot.common.utils.Payload;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	@RequestMapping(value = "/api/test", method = RequestMethod.GET)
-	public TestModel test() {
-		TestModel testModel = new TestModel();
-		testModel.setName("testName");
-		testModel.setId(123);
-		return testModel;
+	public Payload testGet() {
+		return new Payload(123, "Get Mapping Test");
+	}
+	@RequestMapping(value = "/api/test", method = RequestMethod.POST)
+	public Payload testPost(@RequestBody TestModel test) {
+		return new Payload(test.getId(), "Post Mapping Test" + test.getName());
 	}
 }
--- src/main/java/com/example/springboot/common/test/TestModel.java
+++ src/main/java/com/example/springboot/common/test/TestModel.java
@@ -1,10 +1,14 @@
 package com.example.springboot.common.test;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestModel {
 	public String name;
 	public int id;
