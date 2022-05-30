93e58488bb37144257b93ebf0be1b9c1bd5e1626
woohyeoung <dngud951108@gmail.com>
2022-05-30 14:37:17 +0900

    Add: 05/30, Logger info to User-Board

--- src/main/java/com/example/springboot/board/controller/BoardController.java
+++ src/main/java/com/example/springboot/board/controller/BoardController.java
@@ -6,6 +6,8 @@ import com.example.springboot.board.model.BoardUpdateRequestDTO;
 import com.example.springboot.board.service.BoardService;
 import com.example.springboot.common.utils.Response;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,30 +17,38 @@ import java.util.List;
 @RequestMapping("/api/board")
 public class BoardController {
 
+	private final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	private final BoardService boardService;
 
 	@PostMapping("/save")
-	public Integer save(@RequestBody BoardSaveRequestDTO boardSaveRequestDTO) {return boardService.save(boardSaveRequestDTO);}
+	public Response save(@RequestBody BoardSaveRequestDTO boardSaveRequestDTO) {
+		logger.info("BoardController save method ...");
+		return new Response(2, true, boardService.save(boardSaveRequestDTO));
+	}
 
 	@GetMapping("/list")
-	public List<BoardResponseDTO> index() {
+	public Response index() {
+		logger.info("BoardController index method ...");
 		List<BoardResponseDTO> list = boardService.findAllDesc();
-		return list;
+		return new Response(2, true, list);
 	}
 
 	@PutMapping("/list/{id}")
-	public Integer update(@PathVariable(name = "id") Integer boardNo,
+	public Response update(@PathVariable(name = "id") Integer boardNo,
 						  @RequestBody BoardUpdateRequestDTO boardUpdateRequestDTO) {
-		return boardService.update(boardNo, boardUpdateRequestDTO);
+		logger.info("BoardController update method ...");
+		return new Response(2, true, boardService.update(boardNo, boardUpdateRequestDTO));
 	}
 
 	@GetMapping("/list/{id}")
-	public BoardResponseDTO findById(@PathVariable(name = "id") Integer boardNo) {
-		return boardService.findById(boardNo);
+	public Response findById(@PathVariable(name = "id") Integer boardNo) {
+		logger.info("BoardController findById method ...");
+		return new Response(2, true, boardService.findById(boardNo));
 	}
 
 	@DeleteMapping("/list/{id}")
 	public Response delete(@PathVariable(name = "id") Integer boardNo) {
+		logger.info("BoardController delete method ...");
 		boardService.delete(boardNo);
 		return new Response(2, true, boardNo);
 	}
