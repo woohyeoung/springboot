6bb3dbfab201e5ff0d1276dc0ca38a0d073d80b8
woohyeoung <dngud951108@gmail.com>
2022-06-09 17:21:18 +0900

    Fix: scheduler to database #7

--- src/main/java/com/example/springboot/common/config/db/scheduler.sql
+++ src/main/java/com/example/springboot/common/config/db/scheduler.sql
@@ -1,33 +1,33 @@
-# CREATE EVENT del_user
-#     ON SCHEDULE EVERY 1 DAY
-#     DO
-#
-# # 기호 1
-# DELETE FROM tbl_user u
-#     WHERE u.access_date < (
-#     SELECT DATE_SUB(NOW(), INTERVAL 7 DAY) FROM DUAL
-#     );
-#
-# # 기호 2
-# INSERT INTO tbl_unused_id ui
-# (
-#  ui.email, ui.name, ui.access_date
-# )
-# SELECT u.email, u.name, u.access_date
-# FROM tbl_user u
-# WHERE access_date < DATE_SUB(NOW(), INTERVAL 7 DAY);
-#
-# INSERT INTO tbl_unused_pw up
-# (
-#  up.password, up.role
-# )
-# SELECT u.password, u.role
-# FROM tbl_user u
-# WHERE access_date < DATE_SUB(NOW(), INTERVAL 7 DAY);
-#
-# # 기호 3
-# WITH tbl AS
-# (
-#     SELECT DATE_SUB(NOW(), INTERVAL 7 DAY) t FROM DUAL
-# )
-# SELECT * FROM tbl_user u WHERE u.access_date < tbl.t;
+CREATE EVENT del_user
+    ON SCHEDULE EVERY 1 DAY
+    DO
+
+# 기호 1
+DELETE FROM tbl_user u
+    WHERE u.access_date < (
+    SELECT DATE_SUB(NOW(), INTERVAL 7 DAY) FROM DUAL
+    );
+
+# 기호 2
+INSERT INTO tbl_unused_id
+(
+ email, name, access_date
+)
+SELECT u.email, u.name, u.access_date
+FROM tbl_user u
+WHERE access_date < DATE_SUB(NOW(), INTERVAL 7 DAY);
+
+INSERT INTO tbl_unused_pw
+(
+ password, role
+)
+SELECT u.password, u.role
+FROM tbl_user u
+WHERE access_date < DATE_SUB(NOW(), INTERVAL 7 DAY);
+
+# 기호 3
+WITH tbl AS
+(
+    SELECT DATE_SUB(NOW(), INTERVAL 7 DAY) t FROM DUAL
+)
+SELECT * FROM tbl_user u WHERE u.access_date < tbl.t;
